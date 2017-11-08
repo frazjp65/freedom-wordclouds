ccbec5b11b99a86b611725efa6177171d30da356
Joseph Frazer <frazjp65@gmail.com>
2017-11-08 16:06:28 -0500

    initial code

---
+++ README.md
@@ -0,0 +1,11 @@
+Data sources: 
+
+US Declaration of Independence: http://constitution.org/usdeclar.txt
+
+US Constitution: https://www.usconstitution.net/const.txt
+
+Common Sense: http://www.gutenberg.org/cache/epub/147/pg147.txt
+
+The Federalist Papers: http://www.gutenberg.org/cache/epub/18/pg18.txt
+
+Following the guide: https://www.r-bloggers.com/text-mining-and-word-cloud-fundamentals-in-r-5-simple-steps-you-should-know/
---
+++ app.R
@@ -0,0 +1,34 @@
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
+makeWordCloud <- function(inFile, outFile) {
+  text <- readLines(inFile)
+  docs <- Corpus(VectorSource(text))
+  docs <- tm_map(docs, content_transformer(tolower))
+  docs <- tm_map(docs, removeNumbers)
+  docs <- tm_map(docs, removeWords, stopwords("english"))
+  docs <- tm_map(docs, removeWords, c("will", "shall"))
+  docs <- tm_map(docs, removePunctuation)
+  docs <- tm_map(docs, stripWhitespace)
+  
+  dtm <- TermDocumentMatrix(docs)
+  m <- as.matrix(dtm)
+  v <- sort(rowSums(m),decreasing=TRUE)
+  d <- data.frame(word = names(v),freq=v)
+  head(d, 10)
+  
+  set.seed(1234)
+  
+  png(filename=outFile)
+  wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+            max.words=200, random.order=FALSE, rot.per=0.35, 
+            colors=brewer.pal(8, "Dark2"))
+  dev.off()
+}
+
+makeWordCloud('./data/usdeclar.txt', './output/usdeclar.png')
+makeWordCloud('./data/const.txt', './output/const.png')
+makeWordCloud('./data/common-sense.txt', './output/common-sense.png')
+makeWordCloud('./data/federalist.txt', './output/federalist.png')
